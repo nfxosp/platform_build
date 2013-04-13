8cfd88159edcf0197f96e656544b3c8dba922ff1
Steve Kondik <shade@chemlab.org>
2013-04-13 13:23:35 -0700

    envsetup: Make installrecovery work in recovery mode too
    * Needs a change to adb to allow waiting for recovery
    
    Change-Id: Ia90645513f46bcb5f20b7c74c55ef15842710d3d
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1623,9 +1623,8 @@ function installrecovery()
     adb start-server
     adb root
     sleep 1
-    adb wait-for-device
-    adb remount
-    adb wait-for-device
+    adb wait-for-online shell mount /system 2>&1 >> /dev/null
+    adb wait-for-online remount
     if (adb shell cat /system/build.prop | grep -q "ro.nfx.device=$NFX_BUILD");
     then
         adb push $OUT/recovery.img /cache/
